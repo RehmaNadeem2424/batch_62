389433ff2d97d585ef333a0fdc0e4157f41ec8e2
RehmaNadeem <rehmanadeem2424@gmail.com>
2024-08-26 01:16:20 -0700

    first commit

---
+++ calculater.py
@@ -0,0 +1,17 @@
+# ADDITION
+number1 = 17
+number2 = 25
+add_result= number1+number2
+# SUBTRACTION
+number1 = 17
+number2 = 25
+subtract_result= number1-number2
+# MULTIPLICATION
+number1 = 17
+number2 = 25
+multiply_result= number1*number2
+# DIVISION
+number1 = 17
+number2 = 25
+divide= number1/number2
+print(add_result,subtract_result,multiply_result,divide_result)
---
+++ exercise.py
@@ -0,0 +1,81 @@
+# QUESTION 1
+message:str ="hello python people"
+print(message.title())
+# QUESTION 2 
+message:str = "well done is better than well said."
+print(message.title())
+message:str = "life is to short." 
+print(message.title())
+# QUESTION 3 
+r:str = "rehma"
+print(f"{r} would you like to learn python today?".title())
+# QUESTION 4 
+person_name:str = "hafiz nadeem"
+print(person_name.upper())
+print(person_name.lower()) 
+print(person_name.title())
+# QUESTION 5
+quote:str ="failure is a word unknown to me." 
+famous_person:str = "muhammad ali jannah"
+print(f"{famous_person} once said, {quote}")
+# QUESTION 6
+famous_person:str = "allama iqbal"
+message = f"{famous_person} work work and work and we are bound to succeed."
+print(message)
+# QUESTION 7
+person_name:str = "   wahaj   "
+person_name:str = "    hello,\nwahaj   "
+print(person_name.lstrip())
+print(person_name.rstrip())
+print(person_name.strip())
+# QUESTION 8
+x:int = 5
+y:int = 10 
+z:int = 15
+sum = x+y+z
+# QUESTION 9
+a = 10
+b = 20
+print("before swap")
+print("a =",a)
+print("b =",b)
+a,b = b,a
+print("after swap")
+print("a =",a)
+print("a =",b)
+# QUESTION 10
+favourite_colour = "red"
+print(favourite_colour)
+new_colour = favourite_colour
+print(f"{new_colour} ")
+# QUESTIONS 11
+pet_name = "buddy"
+pet_name = "max"
+print(pet_name)
+# QUESTION 12 
+#sunshine = "sunshine"
+#print(sunshine)
+#print(sunsine)
+# QUESTION 13
+score:int = 100
+print(score)
+score:int = 90
+print(score)
+# QUESTION 14 
+city:str = "lahore"
+print(f"city")
+# QUESTION 15
+text:str = "python prgramming"
+print(f"text".title())
+# QUESTION 16
+mixed = "python world"
+print(mixed.lower())
+# QUESTION 17 
+mixedcase:str = "python world"
+print(mixedcase.upper())
+# QUESTION 18
+temperature:int = 25
+print(f"the current temperature is  {temperature} degrees.")
+# QUESTION 19
+poem:str = """love,like a mountain wind upon an oak,falling upon me shakes me leaf and bough."""
+print(poem) 

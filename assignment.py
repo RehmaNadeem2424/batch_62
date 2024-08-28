4645f8b626a8140393647f83a9c0c4f4147ae704
RehmaNadeem <rehmanadeem2424@gmail.com>
2024-08-28 01:24:01 -0700

    new changing

---
+++ assignment.py
@@ -0,0 +1,4 @@
+# STEP 1
+name = (input("enter your name:"))
+print(name)
+# STEP 2:
---
+++ practiceFC.py
@@ -0,0 +1,8 @@
+# PRINT FUNCTION:
+print("my name is rehma")
+print("my age is": , 21)
+print("my favourite colour is red")
+print("i am a brillant student")
+print("i am a food lover")
+print("i love biryani")
+print("")

12c130b19d4a83e92c7b677c4b871fa36edc4e33
RehmaNadeem <rehmanadeem2424@gmail.com>
2024-08-28 08:05:01 -0700

    new file

--- practiceFC.py
+++ practiceFC.py
@@ -1,8 +1,53 @@
 # PRINT FUNCTION:
 print("my name is rehma")
-print("my age is": , 21)
+print("my age is :" , 21)
 print("my favourite colour is red")
 print("i am a brillant student")
 print("i am a food lover")
 print("i love biryani")
-print("")
+print("i am innocent girl")
+print("i live in lahore")
+print("my favourite hobby aram krna")
+print("i want to learn more about this language")
+print("i want to become a good developer")
+print("l like to read books")
+print("living this country is my biggest dream")
+print("i want to go turkey")
+print("turkey is my dreamland country")
+print("i love to read novels")
+print("my favourite fruit is mango")
+print("my favourite drink is sting")
+print("i have no friend")
+print("i like watching dramas")
+print("my aim in my life to learn and gain knowledge about everything")
+print("i am a brave girl")
+print("thank you uzair bhai for helping me")
+print("That's all about me")
+
+# VARIABLES:
+name = "rehma"
+print(name)
+age = 21
+print(age)
+city_name = "lahore , karachi , islamabad"
+print(city_name)
+favourite_place = "turkey"
+print("favourit_place")
+fruits = "banana , apple , grapes"
+print(fruits)
+vegetables = "carrot , raddish , brinjal"
+print(vegetables)
+fav_colour = "red , black"
+print(fav_colour)
+fav_food = "biryani with cock"
+print(fav_food)
+fav_pet = "rabbit , parrot"
+print(fav_pet)
+animals = "elephant , cat , tiger , dog , lion"
+print(animals)
+birds = "peacock , sparrow , crow"
+print(birds)
+message1 = "ap ny bht lmbi assignment de h"
+print(message1)
+message2 = "mai thak gai thi meri typing speed bhi abhi slow h"
+print(message2)
